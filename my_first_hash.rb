4e716b9a60d68cda36732cdab1f723b68d80dcad
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-25 23:06:49 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
+good_dogs = {"big" => "mastiff", "medium" => "beagle", "small" => "shih tzu"}
   
 end
 
